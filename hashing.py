5c47a7b83de3f7045226b881a219c18022df0d47
venky <venkatesh@wealthy.in>
2026-04-05 18:24:38 +0530

    Basic hashing

--- hashing.py
+++ hashing.py
@@ -1,3 +1,5 @@
+
+# int hasing
 def frequency_arr(size, arr, lookup_count, lookup_arr):
     hash_arr = [0 for i in range(13)]
     for i in arr:
@@ -7,11 +9,37 @@ def frequency_arr(size, arr, lookup_count, lookup_arr):
             print(hash_arr[i])
 
 
+# char hasing 
+def freq_arr_char(arr,lookup_arr):
+    c_hash = [0 for i in range(27)]
+    for i in arr:
+        c_index = ord(i)-97
+        c_hash[c_index] = c_hash[c_index] + 1
+    for i in lookup_arr:
+        c_index = ord(i) - 97
+        print(c_hash[c_index])
 
-size = int(input())
-arr =[ int(i) for i in  input().split(" ")]
-lookup_count = int(input())
+
+# Return a list of pairs where each pair contains a unique element from the array and its frequency in the array.
+
+def counting_freq(nums):
+    freq_map = dict()
+    for num in nums:
+        if freq_map.get(num, 0)>0:
+            freq_map[num] = freq_map.get(num) + 1
+        else:
+            freq_map[num] = 1
+
+    arr = list()
+    for k, i in freq_map.items():
+        arr.append([k,i])
+
+    print(arr)
+    return arr
+     
+
+
+
 lookup_arr =[int(i) for i in  input().split(" ")]
-print(lookup_arr)
 
-frequency_arr(size, arr, lookup_count, lookup_arr)
+counting_freq(lookup_arr)
