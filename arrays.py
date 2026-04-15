92e9880da3dff911bc1f6b950be1c9e43df9b0c2
venky <venkatesh@wealthy.in>
2026-04-15 23:05:00 +0530

    Learning array

--- arrays.py
+++ arrays.py
@@ -40,7 +40,17 @@ def remove_dups_optimized(nums):
     return i+1
 
 
+def max_consecutive_ele(nums):
+    count = 0
+    maxi = 0
+    for i in range(len(nums)):
+        if nums[i] == 1:
+            count += 1
+            maxi = max(maxi, count)
+        else:
+            count = 0
+    return maxi
 
 nums = [int(n) for n in input().split()]
 
-print(remove_dups_optimized(nums))
+print(max_consecutive_ele(nums))
